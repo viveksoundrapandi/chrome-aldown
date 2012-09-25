f89978afa4c1044a89a77dc55de17f386d1210df
vivekhas3 <vivekhas3@gmail.com>
2012-09-25 19:43:20 +0530

    chrome extenision
    files to be installed as an extension to chrome

---
+++ fb/chrome/content.js
@@ -0,0 +1,5 @@
+$(function(){
+		   window.location.href="https://www.facebook.com/dialog/oauth?client_id=187689561318469&redirect_uri=http://localhost/aldown.html&response_type=token";
+		  
+
+		   });
